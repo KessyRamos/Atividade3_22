cd19532f07c54efc097bafc5d9624c74b9b643d9
KessyRamos <kessyellenramos@gmail.com>
2022-06-05 09:35:31 -0300

    PrimeiroCommit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿int valorVendido = 500000;
+int porcentagem = 5;
+double resultado = (valorVendido * porcentagem) / 100;
+
+Console.WriteLine("Calcular a comissão do vendedor!!!");
+
+Console.Write($"A comissão que o vendedor irá receber vai ser de: {resultado}");
